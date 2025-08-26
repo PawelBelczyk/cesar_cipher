c87fef45dfc7e8998ab4701c17b31ca731ab3bfa
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2025-08-26 11:58:09 +0200

    swapped if to while, to prevent exit the program when user input not latin letters.

--- cesar_cipher.rb
+++ cesar_cipher.rb
@@ -1,18 +1,13 @@
-# it is cesar cipher,
-# project in ruby
+require 'pry-byebug'
 
 puts 'Enter the word to process'
 input_from_user = gets.chomp
-if input_from_user.match?(/\A[a-zA-Z]+\z/)
-  puts "your word to process is \e[32m#{input_from_user} \e[0m
-  How many  \e[31m shifts \e[0m your word have to made ?"
-else
-  puts 'make sure you use words from latin alphabet!'
+
+while input_from_user !~ /\A[a-zA-Z!.?]+\z/
+  puts 'Make sure you use words from the Latin alphabet and ! . ?'
+  puts 'Enter a word to process:'
+  input_from_user = gets.chomp
 end
 
-shifts = gets.chomp
-if shifts.match?(/\A\d+\z/)
-  shifts.to_i
-else
-  puts 'You must input the Integrer'
-end
+puts "Your word to process is \e[32m#{input_from_user}\e[0m"
+puts "How many \e[31mshifts\e[0m should be applied?"
